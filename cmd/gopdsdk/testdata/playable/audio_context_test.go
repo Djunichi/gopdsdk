c266bdbb09989ee765beafbfe49c1e07e3bd0fa5
Ivan Gromov <nocreative79@gmail.com>
2026-08-02 19:31:19 +0200

    Cleanup test app

--- cmd/gopdsdk/testdata/playable/audio_context_test.go
+++
@@ -1,6 +0,0 @@
-package game
-
-import "github.com/Djunichi/gopdsdk/playdate"
-
-func (*context) LoadSoundEffect(string) (playdate.SoundEffect, error) { return nil, nil }
-func (*context) LoadFilePlayer(string) (playdate.FilePlayer, error)   { return nil, nil }
